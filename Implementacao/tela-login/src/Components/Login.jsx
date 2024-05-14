c2746d8d21a7dc031cc7209453eb22a0c37a6f98
Bruno Henrique <brunobhhf@gmail.com>
2024-05-13 22:30:44 -0300

    tela login feita

--- Implementacao/tela-login/src/Components/Login.jsx
+++ Implementacao/tela-login/src/Components/Login.jsx
@@ -15,22 +15,27 @@ const Login = () => {
 
   return (
     <div className="container">
-      <from onSubmit = {handleSubmit}>
-        <h1>Login</h1>
-        <div className='input-field'>
+      <from className="leftlogin" onSubmit = {handleSubmit}>
+        <div className='cardLogin'>
+          <h1>Login</h1>
+          <div className='input-field'>
             <input type="email" placeholder="E-mail" onChange={(e)=>setUsername(e.target.value)}/>
-        </div>
-        <div className='input-field'>
+          </div>
+          <div className='input-field'>
             <input type="password"placeholder="Senha" onChange={(e)=>setPassword(e.target.value)}/>
-        </div>
-        <button>→</button>
-        <div className='new-account'>
-            <a href='#'>Criar Conta</a>
-        </div>
-        <div className='forget'>
-            <a href='#'>Fazer Login</a>
+            <button>→</button>
+          </div>
+          <div className='links'>
+            <div className='new-account'>
+              <a href='#'>Criar Conta</a>
+            </div>
+            <div className='forget'>
+              <a href='#'>Esqueceu a senha?</a>
+            </div>
+          </div>
         </div>
       </from>
+      <div className='right-login'></div>
     </div>
   )
 };
